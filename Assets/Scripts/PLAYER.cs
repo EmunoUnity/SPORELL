b679c4705f750929a34bbe70995d8f7557f44ebd
Insanitori <natoriwolf2p@gmail.com>
2022-11-09 16:33:40 -0500

    Update PLAYER.cs

--- Assets/Scripts/PLAYER.cs
+++ Assets/Scripts/PLAYER.cs
@@ -6,14 +6,15 @@ public class PLAYER : MonoBehaviour
 {
     // Start is called before the first frame update
     public static float life = 100;
+    public float testlife;
     void Start()
     {
-        
+        testlife = life;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        testlife = life;
     }
 }
