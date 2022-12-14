3408ca9dff4d5fc40debdf8cdb5d29791f887185
Insanitori <natoriwolf2p@gmail.com>
2022-12-14 03:09:40 -0500

    Powerup

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class PowerUp : MonoBehaviour
+    
 {
+    private GameObject[] pain;
+    public ExperienceBar exp;
+
+    private int minRange;
     // Start is called before the first frame update
     void Start()
     {
-        
+        exp = FindObjectOfType<ExperienceBar>();
+        minRange = 20;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E) /*&& ExperienceBar.bars == 1f*/)
+        {
+            pain = GameObject.FindGameObjectsWithTag("Enemy");
+
+            foreach(GameObject go in pain)
+{
+                float distance = Vector3.Distance(transform.position, go.transform.position);
+
+                if(distance < minRange)
+                {
+                    go.GetComponent<EnemyNav>().enemyLife -= 200;
+                    
+                }
+            }
+        }
     }
 }
